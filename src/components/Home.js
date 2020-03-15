80eeee850892642e6a4cfd54a7c7f09eb0488191
Fernando Cordeiro <fernando.cordeiro@jeronimo-martins.com>
2020-03-15 14:01:04 +0000

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="main-content home">
+        <h2>Front End Course Directory</h2>
+        <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
+        <hr />
+
+      </div>
+    );
+  }
+}
+
+export default Home;
